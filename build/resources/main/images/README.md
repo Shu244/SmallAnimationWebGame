70da0af12eff605aead687bff297b8b94772ff83
Shuhao Lai <ShuhaoLai18@gmail.com>
2020-03-19 20:37:02 -0400

    Finished game

---
+++ build/resources/main/images/README.md
@@ -0,0 +1,6 @@
+# Summary
+* The owlet and dude have tiles that are 32 x 32 px. 
+* The dude is the player while the owlet is the monster.
+# Credit
+* Thanks to craftpix.net for their free sprites.
+* Site url: https://craftpix.net/freebies/free-pixel-art-tiny-hero-sprites/ 
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -27,12 +27,14 @@ public class Main {
      */
     public static void main(String[] args) {
         Javalin app = Javalin.create(config -> {
-            config.addStaticFiles("");
+            config.addStaticFiles("css/");
+            config.addStaticFiles("images/");
+            config.addStaticFiles("js/");
         });
 
         app.get("/", (ctx) -> {
             ctx.status(200);
-            ctx.render("index.html");
+            ctx.render("html/index.html");
         });
         app.start(7000);
     }
